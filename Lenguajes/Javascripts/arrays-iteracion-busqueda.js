05affc8cb7dc87971cf40ddd3c17028b7a4d2956
AlejandroV <alejandrovelardenahui.02@gmail.com>
2023-10-05 22:47:03 -0500

    se grabo aca

--- Lenguajes/Javascripts/arrays-iteracion-busqueda.js
+++ Lenguajes/Javascripts/arrays-iteracion-busqueda.js
@@ -1,2 +1,27 @@
 
-console.log("hola mundo");
+let frutas = [1, 2, 3, 4]
+
+// frutas.splice(1,1,2,2)
+
+
+let otro = frutas.map(let => {
+    return let = let*5
+  }
+)
+
+let nuevo = frutas.filter(element =>{
+    return element % 2 == 0
+  }
+)
+
+let litro = frutas.find(element => {
+    return element % 2 == 0
+  }
+)
+
+console.log(frutas);
+console.log(otro);
+console.log(nuevo);
+console.log(litro);
+
+
